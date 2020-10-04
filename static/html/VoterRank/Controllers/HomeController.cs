b7d695bb45a2763958b8aad1dc944a73427098f7
Eric Perez <eric.t.perez1@outlook.com>
2020-10-04 18:28:53 -0500

    Initial ASP.net commit/Op, still requires DBcontext 10/4

---
+++ static/html/VoterRank/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using VoterRank.Models;
+
+namespace VoterRank.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetStarted()
+        {
+            return View();
+        }        
+        public IActionResult FAQ()
+        {
+            return View();
+        }        
+        public IActionResult SignIn()
+        {
+            return View();
+        }        
+        public IActionResult ThankYou()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ static/html/VoterRank/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VoterRank.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
